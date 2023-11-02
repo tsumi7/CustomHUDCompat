15fdaaddacfa93ba81939aeac63a8c2c8c4db673
tsumi <who@asked>
2023-11-02 06:29:46 +0100

    make mega jar work

--- src/main/java/we/love/casting/spells/CustomHUD/LitematicaCompat/Loader.java
+++
@@ -1,9 +0,0 @@
-package we.love.casting.spells.CustomHUD.LitematicaCompat;
-import net.fabricmc.api.ModInitializer;
-
-public class Loader implements ModInitializer {
-	@Override
-	public void onInitialize() {
-        Layer.Load();
-    }
-}
